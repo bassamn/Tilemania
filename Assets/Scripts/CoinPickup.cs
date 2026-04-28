15d2b4be6c5e1b5dd097856afc65e009fc504121
Bassam Nazemi <bassamna@proton.me>
2026-04-28 12:35:58 +0330

    Change coin pickup system to tilemap
    Now coins are tiles
    CoinPickup script file has changed to match new coin pickup system

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -1,17 +1,38 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickupSFX;
+    [SerializeField] Tilemap coinTilemap;
 
-    bool isCollected = false;
-
-    void OnTriggerEnter2D(Collider2D collision) 
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        if (isCollected == true) {return;}
+        // Get the bounds of the player's collider
+        Bounds playerBounds = collision.bounds;
         
-        isCollected = true;
-        AudioSource.PlayClipAtPoint(coinPickupSFX, transform.position);
-        Destroy(gameObject);
+        // Convert to cell coordinates
+        Vector3Int minCell = coinTilemap.WorldToCell(playerBounds.min);
+        Vector3Int maxCell = coinTilemap.WorldToCell(playerBounds.max);
+        
+        // Iterate through all cells the player overlaps
+        for (int x = minCell.x; x <= maxCell.x; x++)
+        {
+            for (int y = minCell.y; y <= maxCell.y; y++)
+            {
+                Vector3Int cellPosition = new Vector3Int(x, y, 0);
+                
+                if (coinTilemap.HasTile(cellPosition))
+                {
+                    coinTilemap.SetTile(cellPosition, null);
+                    
+                    if (coinPickupSFX != null)
+                    {
+                        Vector3 tileWorldPos = coinTilemap.GetCellCenterWorld(cellPosition);
+                        AudioSource.PlayClipAtPoint(coinPickupSFX, tileWorldPos);
+                    }
+                }
+            }
+        }
     }
 }
