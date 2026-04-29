5df85497fe1c9ec538afd0cc0f39be6602485c48
Bassam Nazemi <bassamna@proton.me>
2026-04-29 10:34:42 +0330

    Update UI
    Move collected coin to left
    Add exit button
    Change size of UI elements

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
